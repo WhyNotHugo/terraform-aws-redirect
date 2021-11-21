6e7669ac251b221cc46348b393cd1c031a0ec34e
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-11-21 17:56:01 +0100

    Remove unused bits of the function

--- function.js
+++ function.js
@@ -1,15 +1,11 @@
-const querystring = require('querystring');
-
 // Note: querystrings are dropped.
 function handler(event) {
   var targetDomain = "${new_domain}";  // Filled in by terraform when templating.
-  var newUrl = "https://" + targetDomain + "/" + event.request.uri;
+  var newUrl = "https://" + targetDomain + event.request.uri;
 
-  const response = {
+  return {
     statusCode: 302,
     statusDescription: "Found",
     headers: { location: { value: newUrl } },
   };
-
-  return response;
 }
